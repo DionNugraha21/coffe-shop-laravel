9a82a62330b9a4bba5bf787fe4396ea276c43065
DionNugraha21 <dionnug@gmail.com>
2022-10-03 10:59:59 +0700

    Selesai Modul 1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RoleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,4 +22,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+// Route::resource('roles', RoleController::class);
+Route::controller(RoleController::class)->group(function () {
+    // Route::get('roles', 'index')->name('roles')->middleware('can:read role');
+    Route::get('roles', 'index')->name('roles');
+});
